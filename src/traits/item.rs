fe1f0b69e5580096b01faad8fecca39433de4970
Jörg Sischka <j.sischka@kochmedia.com>
2022-06-27 15:26:44 +0200

    test case added

--- src/traits/item.rs
+++ src/traits/item.rs
@@ -2,12 +2,12 @@ use crate::errors::Result;
 
 use super::shared::Divider;
 
-pub trait DivideUsing<S: Divider> {
+pub trait DivideUsing<D: Divider> {
     type ITEM;
 
     fn divide_using(
         &self,
-        divider: &S,
+        divider: &D,
         dst_left: &mut Self::ITEM,
         dst_right: &mut Self::ITEM,
     ) -> Result<()>;
